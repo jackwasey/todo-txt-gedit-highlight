028fdf1270a9bebcf587b59d1e5c8acaab2cc282
Jack Wasey <jack@jackwasey.com>
2020-05-15 14:30:41 -0400

    sourceview 4 path

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 SRC=$PWD
-mkdir -p ~/.local/share/gtksourceview-4.0/language-specs/
-cd ~/.local/share/gtksourceview-4.0/language-specs/
+mkdir -p ~/.local/share/gtksourceview-4/language-specs/
+cd ~/.local/share/gtksourceview-4/language-specs/
 ln -sf $SRC/todotxt.lang
 mkdir -p ~/.local/share/gtksourceview-3.0/language-specs/
 cd ~/.local/share/gtksourceview-3.0/language-specs/
