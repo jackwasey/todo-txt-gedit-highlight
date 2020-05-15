e0677f4a9e4fa4c4b969bd44a5e8240dc556217b
Jack Wasey <jack@jackwasey.com>
2020-05-15 14:23:44 -0400

    needs latest dir for install. comments with #
    https://developer.gnome.org/gtksourceview/stable/lang-reference.html

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 SRC=$PWD
+mkdir -p ~/.local/share/gtksourceview-4.0/language-specs/
+cd ~/.local/share/gtksourceview-4.0/language-specs/
+ln -sf $SRC/todotxt.lang
 mkdir -p ~/.local/share/gtksourceview-3.0/language-specs/
 cd ~/.local/share/gtksourceview-3.0/language-specs/
-ln -s $SRC/todotxt.lang
+ln -sf $SRC/todotxt.lang
 mkdir -p ~/.local/share/gtksourceview-2.0/language-specs/
 cd ~/.local/share/gtksourceview-2.0/language-specs/
-ln -s $SRC/todotxt.lang
+ln -sf $SRC/todotxt.lang
